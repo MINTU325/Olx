39b64af421110e9cb065ebb5f6ce47155b59949d
MINTU325 <sainimintu34@gmail.com>
2021-07-25 13:48:45 +0530

    CHat function

--- app/src/main/java/com/example/olx/bikes/ItemClickListenerBike.java
+++ app/src/main/java/com/example/olx/bikes/ItemClickListenerBike.java
@@ -1,6 +1,12 @@
 package com.example.olx.bikes;
 
+import retrofit2.Call;
+
 public interface ItemClickListenerBike {
     void OnItemClickedBike(Bikes bikes, int position);
     void OnLikeClickedBike(Bikes bikes, int position);
 }
+
+
+
+
